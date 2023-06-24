de52ae8246faf9578b3ba6dbad17fc7a47d39886
Oleg <Oleg@DESKTOP-B5TOANR>
2023-06-25 00:36:50 +0300

    Project init

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,25 @@
+<nav class="grid grid-cols-12 p-5 items-center">
+    <div class="col-span-3">
+        <h1 class="text-2xl font-bold">TechGet</h1>
+    </div>
+    <div class="col-span-6">
+        <form action="post" class="w-full relative">
+            <input class="w-full input" type="text" placeholder="Поиск">
+            <button class="absolute top-1/2 right-0 -translate-y-1/2">
+                <span class="material-symbols-outlined">search</span>
+            </button>
+        </form>
+    </div>
+    <div class="col-span-3">
+        <div class="flex justify-end">
+            <button class="mr-3 flex flex-col">
+                <span class="material-symbols-outlined">shopping_cart</span>
+                Корзина
+            </button>
+            <button class="flex flex-col">
+                <span class="material-symbols-outlined">account_circle</span>
+                Войти
+            </button>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    sasi
+</x-app-layout>
